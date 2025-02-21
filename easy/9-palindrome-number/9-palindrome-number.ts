fc82e6c79f5b2dc3c953d688185e5a52fb12d83b
Artem Turlenko <76534386+art2url@users.noreply.github.com>
2025-02-21 16:38:20 +0100

    Add solution for 9. Palindrome Number

---
+++ easy/9-palindrome-number/9-palindrome-number.ts
@@ -0,0 +1,10 @@
+function isPalindrome(x: number): boolean {
+    if (x < 0) return false;
+    let original = x;
+    let reversed = 0;
+    while (x > 0) {
+        reversed = reversed * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+    return original === reversed;
+}
